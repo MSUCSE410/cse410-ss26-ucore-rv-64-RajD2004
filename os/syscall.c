fa594dbe8dc166c3c860d7cda4fd6dc9ea02d3c7
Sairaj Goutham Dhullipalla <dhullipa@msu.edu>
2026-02-18 22:07:14 +0000

    commit

--- os/syscall.c
+++ os/syscall.c
@@ -39,6 +39,21 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+int sys_task_info(TaskInfo* ti){
+	struct proc* p = curr_proc();
+
+	if(!ti) return -1;
+
+	ti->status = Running;
+
+	for(int i = 0; i < 500; i++){
+		ti->syscall_times[i] = p->syscall_times[i];
+	}
+
+	ti->time = (get_cycle() - p->start_time) / (CPU_FREQ / 1000);
+	
+	return 0;
+}
 
 extern char trap_page[];
 
@@ -53,6 +68,8 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	curr_proc()->syscall_times[id]++;
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +86,11 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+
+	case SYS_taskinfo:
+		ret = sys_task_info((TaskInfo *) args[0]);
+		break;
+
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
